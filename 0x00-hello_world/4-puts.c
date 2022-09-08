bb1107b69db661c69c150e5b778d2ab7dc0e18c4
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-08 16:44:03 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main(void) -A C program that prints using puts() and ends with value 0
+ * @void: no argumen
+ *
+ * main Description: program that prints exactly Programming is like building a
+ * multilingual puzzle, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
