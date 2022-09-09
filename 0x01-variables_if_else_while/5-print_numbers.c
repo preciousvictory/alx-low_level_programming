3e8c281aa4e944ed405bd460836524695750316a
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-09 15:04:42 -0700

    while, digits 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 10)
+	{
+		printf("%d", i)
+	}
+	putchar('\n')
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/default.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -description
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	return (0);
+}
