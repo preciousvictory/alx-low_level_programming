4d321610bf0dacf2f9a07ab7daf4bc2b778459e6
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-08 17:17:58 -0700

    prinf; bettay again?

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints string using printf followed by newline.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
