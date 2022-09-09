d1a6ba39986cb671f9dd913d19fbbb5cdbe8c7fa
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-08 18:05:12 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Prints followed by new line to the standard error.
+ *
+ * Return: 0 (success) 1 (fail)
+ */
+int main(void)
+{
+	and that piece of art is useful" - Dora Korpar, 2015-10-19;
+	return(0);
+}
