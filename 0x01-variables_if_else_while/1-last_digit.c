dd03b29e16f00e661dd121dfa7dd55c7dc225ad8
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-09 13:13:58 -0700

    if, else if, else if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,16 +15,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
-	if (n > 5)
+	if (ld > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else if (n == 0)
+	else if (ld == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
-	else if ((n < 6) && (n != 0))
-	{
+	else if ((ld < 6) && (ld != 0))
+	}
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
 	return (0);
