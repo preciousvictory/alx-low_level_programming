e9a9ae1cfe66d527e111704fde266cbc9b129ad2
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-09 14:44:30 -0700

    while if, lowercase letter except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -prints all letter from a to z excep letter ( q, e )
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if ((c != 'q') && (c != 'e'))
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
