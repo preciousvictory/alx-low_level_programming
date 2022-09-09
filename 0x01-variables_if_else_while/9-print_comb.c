6b7637a1664a8a9dde26d282cc5dd8464c28818c
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-09 16:01:12 -0700

    while, single digit + , + SPACE

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -prints all single digit numbers of base 10 starting from 0
+ *
+ * separated by ,
+ * Numbers should be printed in ascending order
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar(48 + i);
+		putchar(44)
+		putchar(32)
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
