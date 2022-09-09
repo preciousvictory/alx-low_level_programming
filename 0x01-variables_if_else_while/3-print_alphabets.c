f06610d4b2098f483d9411e9c6b9f58af7857e5d
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-09 14:27:59 -0700

    while, lowercase and uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -prints all letter from a to z and A to Z then a newline
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	char u;
+
+	c = 'a';
+	u = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (u <= 'Z')
+	{
+		putchar(u);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
