2c5be87a4ccddc57ffba3d833939d7ad27ff51b3
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-08 17:30:25 -0700

    sizeof()

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the size of various types on the computer it is compiled.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(i);
+	printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(c);
+	printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(li);
+	printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(lli);
+	printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(f);
+	return (0);
+}
