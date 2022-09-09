dca6edc6cf9c60c73dd8d4b2f8c03dcff69eb0b0
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-08 17:00:37 -0700

    put; betty why?

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- * main(void) -A C program that prints using puts() and ends with value 0
- * @void: no argumen
+ * main -A C program that prints using puts() and ends with value 0
  *
- * main Description: program that prints exactly Programming is like building a
- * multilingual puzzle, followed by a new line.
- * Return: 0
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
