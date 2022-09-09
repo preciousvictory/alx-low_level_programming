93d9671cef0e9fa5aedef3cf475d919924b6eefd
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-09 15:50:13 -0700

    while, hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -prints ll the numbers of base 16 in lowercase
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	c = 'a';
+	while (i <= 10)
+	{
+		putchar(c);
+		i++;
+	}
+	while (c <= 'f')
+	{
+		putchar(u);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
