a3a90c1049f84e30ccbe144d62ee3f06ea110a99
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-09 15:43:51 -0700

    while, lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -prints all letter from a to z in reverse
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while (c > 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
