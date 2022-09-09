489b72f550b2460e66646fcd18c38458dd07a5bc
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-08 17:37:38 -0700

    sizeof()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
-
-	printf("Size of an int: %lu byte(s)\n", (unsigned int)sizeof(i));
+	
 	printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned int)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned int)sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned int)sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", (unsigned int)sizeof(f));
