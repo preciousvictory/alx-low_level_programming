cfcfa95a7c5f13deee21cceec79a73bd7c06e2cb
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-12 03:44:55 -0700

    print_combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,43 +6,44 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+int a;
+int b;
+int c;
+int d;
 
-	a = 0;
-	while (a < 0)
+a = 0;
+while (a < 0)
+{
+	b = 0;
+	while (b < 10)
 	{
-		b = 0;
-		while (b < 10)
+		c =  0;
+		while (c < 10)
 		{
-			c =  0;
-			while (c < 10)
+			d = 0;
+			while (d < 10)
 			{
-				d = 0;
-				while (d < 10)
+				if (!(a == d && b == c))
 				{
-					if (!(a == d && b == c))
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					if (a + b != 17 && d + c != 18 && d != 9)
 					{
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(' ');
-						putchar(c + '0');
-						putchar(d + '0');
-						if (a + b != 17 && d + c != 18 && d != 9)
-						{
-							putchar(',');
+						putchar(',');
 							putchar(' ');
-						}
 					}
-					d++;
 				}
-				c++;
+				d++;
 			}
-			b++;
-		}
-		a++;
+		c++;
+			}
+		b++;
 	}
-	return (0);
+	a++;
+}
+putchar('\n');
+return (0);
 }
