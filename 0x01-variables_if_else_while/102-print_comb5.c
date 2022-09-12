f4861ccf2b23d7f63408c74d661d7da60d99c08b
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-12 03:31:41 -0700

    print_combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints all single digit numbers of base 10 starting from 0
- * 
+ *
  * Return: 0 (success)
  */
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 	int b;
 	int c;
 	int d;
-	
+
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
@@ -19,14 +19,14 @@ int main(void)
 			{
 				for (d = 0; d < 10; d++)
 				{
-					if (!(a == c && b != d && (a + b) < (c + d)))
+					if (!(f == c && e == d))
 					{
 						putchar(a + '0');
 						putchar(b + '0');
 						putchar(' ');
 						putchar(c + '0');
 						putchar(d + '0');
-						if (!(a + b == 17 && c + d == 18 && d == 9))
+						if (!(a + d == 17 && b + c == 18 && d == 9))
 						{
 							putchar(',');
 							putchar(' ');
