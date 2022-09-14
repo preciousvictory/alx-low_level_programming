0e6280b71d1254b5fb4b3551a23c636ed6e24301
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-14 16:18:42 -0700

    24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/** 
+ * jack_bauer - (void)
+ * prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Return: 0 (success)
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k < 5; k**)
+			{
+				for (l =0; l <= 9; l++)
+				{
+					_putchar(i)
+					_putchar(j)
+					_putchar(:)
+					_putchar(k)
+					_putchar(l)
+					putchar('\n')
+				}
+			}
+		}
+	}
+}
