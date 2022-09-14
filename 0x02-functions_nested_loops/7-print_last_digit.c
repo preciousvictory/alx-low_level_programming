5f8976842ccc88b84fafece7ab3a0718d2b2b15a
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-14 06:58:21 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_last_digit -  prints the last digit of a number.
+ * @n: value to be checked
+ *
+ * Return: 0 (success)
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	a = n % 10;
+
+	return (a);
+}
