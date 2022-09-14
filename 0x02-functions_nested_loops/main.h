c03e03be02cf6fbea86cd95fed24518c8c9951b8
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-14 02:17:21 -0700

    alphabet x 10 functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * HEADER FILES
+ * Once-only header- If a header file happens to be included twice,
+ * the compiler will process its contents twice and it will result in an error
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int main(void);
+
+#endif
