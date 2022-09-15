dd3130543b846eecb7791ec0d8c0312808c0eac8
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-15 07:22:37 -0700

    positive negaative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 void positive_or_negative(int n);
@@ -7,4 +7,4 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif
+#endif /* MAIN_H */
