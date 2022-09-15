766b7945ceec1e333957099eb9a47ddd7073ec79
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-15 05:08:10 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,10 +23,10 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i <=98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			if (i != 98) 
+			if (i != 98)
 			{
 				printf(", ");
 			}
