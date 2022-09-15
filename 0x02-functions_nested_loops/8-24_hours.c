3490de033b5bca06b9a806ec55f7ddeb2d068ae0
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-14 17:26:17 -0700

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,9 +15,9 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 3; j++)
 		{
-			for (k = 0; k < 5; k++)
+			for (k = 0; k <= 5; k++)
 			{
 				for (l = 0; l <= 9; l++)
 				{
