b9c87d9e1296e34fa30f3ccc076eb684cb78b5c4
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-16 01:31:50 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - a program that prints the numbers from 1 to 100
+ * followed by a new line. But for multiples of three print Fizz
+ * and for the multiples of five print Buzz.
+ * numbers which are multiples of both three and five print FizzBuzz
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz")
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz")
+		}
+		else
+			_putchar(n)
+	}
+	_putchar('\n');
+
+	return (0);
+}
