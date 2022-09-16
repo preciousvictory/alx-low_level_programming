dee26f9889a8549b230173a950e83ca4cd0503f1
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-16 11:28:50 -0700

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table -  a function that prints the n times table, starting with 0.
+ * @n: The number
+ */
+
+void print_times_table(int n)
+{
+	int i, j, m;
+
+	if (!(n > 15 || n < 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				m = i * j;
+
+				if (m > 9)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(48 + m / 10);
+					putchar(48 + m % 10);
+				}
+				else
+				{
+					if (!(j == 0))
+					{
+						putchar(',');
+						putchar(' ');
+						putchar(' ');
+					}
+					putchar(48 + m);
+				}
+			}
+		}
+	}
+}
