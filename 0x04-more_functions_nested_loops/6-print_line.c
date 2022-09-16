be07b27d821a0b5b8bd628e327e98c716bdb6321
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-15 18:30:55 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - a function that draws a straight line in the terminal.
+ * @n: length of line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
