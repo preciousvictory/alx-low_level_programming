2e6e2b5f641559f33173704f9493f3a864da78b6
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-16 00:19:28 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a diagonal line on the terminal.
+ * @n: length of diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			j = 1;
+
+			while (j == i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+}
