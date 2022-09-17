f39025d676e0b165dcf27554f5af5ffc22e7a88f
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-17 01:30:34 -0700

    fibonacci 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - a program that prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return:  0 (success).
+ */
+int main(void)
+{
+	int i, num_1, num_2, sum;
+
+	num_1 = 1;
+	num_2 = 2;
+	prinf("%d, %d, ", num_1, num_2);
+
+	for (i = 0; i < 50; i++)
+	{
+		sum = num_1 + num_2;
+		prinf("%d, ", sum)
+		num_1 = num_2;
+		num_2 = sum;
+	}
+	return (0);
+}
