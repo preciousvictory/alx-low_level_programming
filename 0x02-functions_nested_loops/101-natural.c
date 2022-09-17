4d6826e1bad5ee8697dbf66462008d8b314ee074
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-16 22:58:53 -0700

    multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * sum_of_multiples - a program that computes and prints the sum of all the
+ * multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ * @n: The number
+ */
+
+void sum_of_multiples(int n)
+{
+	int i, sum;
+
+	for (i = 0; i < n; i++)
+	{
+		sum = 0;
+
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+}
+
+
+/**
+ * main - run the program
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	sum_of_multiples(1024);
+	putchar('\n');
+
+	return (0);
+}
