3d3e9a300eb64d7540ea63258c1096e721637874
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-19 08:54:03 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: The string
+ *
+ * Return: returns the length of string
+ */
+
+int _strlen(char *s)
+{
+	int i, count;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count += 1;
+
+		s++;
+	}	
+
+	return (count);
+}
