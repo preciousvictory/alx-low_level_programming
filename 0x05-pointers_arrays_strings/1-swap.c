af283362552e9f5f12fdb8579005d8ed95e1e5c7
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-19 06:58:22 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int -  a function that swaps the values of two integers.
+ * @a: pointer variable
+ * @b: pointer variable
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
