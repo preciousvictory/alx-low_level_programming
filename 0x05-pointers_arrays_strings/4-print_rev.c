d7cb2ac128452f655ef83d14255dda9a044fc45c
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-19 11:30:48 -0700

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string, followed by a 
+ * print_rev - a function that prints a string, followed by a
  * new line, to stdout.
  * @s: the character
  */
@@ -16,7 +16,7 @@ void print_rev(char *s)
 	}
 
 	length = i;
-	while (length != 0)
+	while (length !< 0)
 	{
 		write(1, &s[length], 1);
 		length--;
