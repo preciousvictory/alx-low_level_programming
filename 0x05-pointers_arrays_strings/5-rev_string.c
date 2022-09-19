cee61bc81e043950340ccc935cfe6ec28969f8cb
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-19 13:37:16 -0700

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,21 +2,23 @@
 
 /**
  * rev_string -  a function that reverses a string.
- * @s - the character
+ * @s:  the character
  */
 
 void rev_string(char *s)
 {
-	int i;
+	int i, j;
 
 	i = 0;
 	while (s[i])
 	{
 		i++;
 	}
+	j = -
 	while (i--)
 	{
-		*s = s[i];
+		*s[j] = s[i];
+		j++;
 	}
 	_putchar('\n');
 }
