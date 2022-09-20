e8f6ec4b6bd35698025b0bbd80a2c4ab06d2a105
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-20 02:56:15 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,16 @@
 
 void print_number(int n)
 {
+
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
-	print_number(n / 10);
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
 
 	_putchar(48 + n % 10);
 }
