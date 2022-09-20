aeecb90a04a3c5f96f1e943c746059d932cfccbf
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-20 03:49:17 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - a program that finds and prints the first 98 Fibonacci numbers, 
+ * main - a program that finds and prints the first 98 Fibonacci numbers,
  * starting with 1 and 2, followed by a new line.
  *
  * Return: 0 (success)
@@ -13,16 +13,16 @@ int main(void)
 
 	num_1 = 1;
 	num_2 = 2;
-	printf("%ld, %ld, ", num_1, num_2);
+	printf("%lld, %lld, ", num_1, num_2);
 
 	for (i = 0; i < 96; i++)
 	{
 		sum = num_1 + num_2;
 
 		if (i == 96)
-			printf("%ld", sum);
+			printf("%lld", sum);
 		else
-			printf("%ld, ", sum);
+			printf("%lld, ", sum);
 
 		num_1 = num_2;
 		num_2 = sum;
