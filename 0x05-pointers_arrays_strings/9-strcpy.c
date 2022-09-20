b613e204fbb00b058880e923ea00b51467ad1e54
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-19 18:11:01 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,13 +14,14 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
+	if (*(src + i) == '\0')
+	{
+		break;
+	}
+
 	for (i = 0; src[i] != '\0' ; i++)
 	{
-		if (*(src + i) == '\0')
-		{
-			break;
-		}
 		dest[i] = src[i];
-	}	
+	}
 	return (dest);
 }
