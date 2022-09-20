807d8611d5cb50bd060a0128d0b99e5c1f9578ef
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-20 16:15:04 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,24 @@
 #include "main.h"
+/**
+ * _pow - To find the power of a number raised to another
+ * @a: base
+ * @b: power
+ *
+ * Return: returns the answer
+ */
+
+int _pow(int a, int b)
+{
+	int i, sol = 1;
+
+	i = b - 1;
+	while (i != 0)
+	{
+		sol = sol * a;
+		i--;
+	}
+	return (sol);
+}
 
 /**
  * _atoi - a function that convert a string to an integer.
@@ -7,18 +27,37 @@
  * Return: the integer
  */
 
-int _atoi(char *s);
+int _atoi(char *s)
 {
-	int num, i, sign;
-	char c[];
+	int i, sign, j, num, conv, sum;
 
+	j = 0;
 	sign = 1;
-	for (i = 0; s[i] >= 48 && s[i] <= 57; i++)
-	{	
+	for (i = 0; s[i] != '\0'; i++)
+	{
 		if (s[i] == '-')
 		{
-			sign = -1;			
+			sign *= -1;
 		}
-		
+
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			j++;
+		}
 	}
+
+
+	sum = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			conv = (int)s[i] - 48;
+
+			num = conv * _pow(10, j);
+			sum += num;
+			j--;
+		}
+	}
+	return (sum * sign);
 }
