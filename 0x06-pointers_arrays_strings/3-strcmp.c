1cdb1ee5e2d9656857920bd63058dd476c5dbc9b
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-21 16:54:17 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * int _strcmp(char *s1, char *s2) - a function that compares two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Description: the function compares s1 and s2 strings character by character
+ * 
+ * Return: --
+ * if the first non-matching character in s1 is greater in ASCII than of s2.
+ * if the first non-matching character in s1 is lower (in ASCII) than of s2.
+ * if strings are equal
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, r;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+		{
+			i++;
+			if (*(s1 + i + 1) == '\0')
+			{
+				r = 0
+			}
+		}
+		else
+			break;
+	}
+
+	if (s1[i] > s2[i])
+	{
+		r = 15;
+	}
+	else
+		r = -15;
+
+	return (r);
+}
