6fd1a2c3ff22f11932c6222144d10f858bf048f9
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-21 13:35:04 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string.
+ * @dest: first string
+ * @src: second string
+ * @n: number of character from src to be added to dest
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest[i] = dest[i];
+	}
+
+	i = 0;
+	while (i != n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
