60b9c884abfa72e75fdf5fed2a3fa499d9dc49c0
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-21 14:21:19 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,17 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i != n && dest[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-
-	while (i != n)
+	
+	while (i < n)
 	{
 		dest[i] = '\0';
-
-		if (src[i] == '\0')
-			break;
 
 		i++;
 	}
