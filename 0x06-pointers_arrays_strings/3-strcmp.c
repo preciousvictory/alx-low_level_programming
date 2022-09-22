c1bbc8bc927ab0af8f3b29e3bd05cdcefa8e2fb2
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-21 17:24:05 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,21 +16,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, r;
 
+	r = *s1 - *s2;
 	i = 0;
 	while (s1[i] == s2[i])
 	{
-		r = 0;
 		i++;
 		if (s1[i] == '\0')
 			break;
 	}
-
-	if (s1[i] > s2[i])
-	{
-		r = 15;
-	}
-	else if (s1[i] < s2[i])
-		r = -15;
 
 	return (r);
 }
