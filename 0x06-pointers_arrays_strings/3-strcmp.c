30e418deb714a8cd348049b41e577d24ce9a5b18
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-21 17:48:30 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,16 +14,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, r;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	r = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		r = *s1 - *s2;
 		i++;
 	}
 
-	if (*s1 == *s2)
-		return (0);
-	else
-		return (*s1 - *s2);
+	return (r);
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -11,9 +11,17 @@ int main(void)
 	char s1[] = "Hello";
 	char s2[] = "World!";
 
+	char s3[] = "hello";
+	char s4[] = "horld!";
+	char s5[] = "wello";
+	char s6[] = "aorld!";
+
 	printf("%d\n", _strcmp(s1, s2));
 	printf("%d\n", _strcmp(s2, s1));
 	printf("%d\n", _strcmp(s1, s1));
+	printf("%d\n", _strcmp(s3, s4));
+	printf("%d\n", _strcmp(s5, s6));
+
 
 	return (0);
 }
