017703eaee3f65a4ce691af4b1677bee10a183e5
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-22 04:55:17 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,24 +14,22 @@
 
 char *leet(char *s)
 {
-	int i;
-	
+	int i, n;
+	char char_[] = "aAeEoOtTlL";
+	char rep[] = "4433007711";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = 4;
-
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = 3;
-
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = 0;
-
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = 7;
-
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = 1;
+		n = 0;
+		while (char_[n] != '\0')
+		{
+			if (s[i] == char_[n])
+			{
+				s[i] = rep[n];
+			}
+			n++;
+		}
 	}
+
 	return (s);
 }
