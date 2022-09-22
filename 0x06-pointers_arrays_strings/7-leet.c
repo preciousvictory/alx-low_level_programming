d8553da6b6e2c87f4b0c69de54244137f1fa510a
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-22 04:26:43 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet -  a function that encodes a string into 1337.
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ * @s: the string
+ *
+ * Return: s the encoded string
+ */
+
+char *leet(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = 4;
+		else if (s[i] == 'e' || s[i] == 'E')
+			s[i] = 3;
+		else if (s[i] == 'o' || s[i] == 'O')
+			s[i] == 0;
+		else if (s[i] == 't' || s[i] == 'T')
+			s[i] = 7;
+		else if (s[i] == 'l' || s[i] == 'L')
+			s[i] = 1;
+	}
+	return (s);
+}
