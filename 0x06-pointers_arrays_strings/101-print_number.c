590aec9b8ce6e1b0cfc6a9a34dfb4d41cddbb6d2
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-22 05:40:11 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: the number
+ */
+
+void print_number(int n)
+{
+	unsigned int _n;
+	/* char num = "0123456789"; */
+
+	if (n < 0)
+	{
+		_n = -n;
+	}
+
+	else
+	{
+		_n = n;
+	}
+
+	if (_n >= 10)
+	{
+		print_number(_n / 10);
+	}
+
+	_putchar('0' + (_n % 10));
+}
