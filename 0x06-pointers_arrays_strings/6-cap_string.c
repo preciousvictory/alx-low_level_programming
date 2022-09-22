d6a1eb0487a4a702ea8d3785330f9a9ababedc68
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-21 19:08:37 -0700

    capstring

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: string
+ * Separators of words: space, tabulation, new line, ,, ;, ., !, ?,
+ * \", (, ), {, and }
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[] = "\t\n,;.!? \" (){}"
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				{
+					s[i + 1] = s[i + 1] - 32
+				}
+			}
+		}
+	}
+
+	return (s);
+}
