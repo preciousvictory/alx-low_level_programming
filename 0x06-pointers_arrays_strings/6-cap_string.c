d19439bac82445922008a3b4c44118c0b3f3d750
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-21 19:31:37 -0700

    capstring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,19 +11,26 @@
 
 char *cap_string(char *s)
 {
-	int i, j;
+	int i, j, do_;
 	char sep[] = "\t\n,;.!? \" (){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; sep[j] != '\0'; j++)
+		if (i == 0)
 		{
-			if (s[i] == sep[j])
+			if (s[i] >= 97 && s[i] <= 122)
+				s[i] = s[i] - 32;
+		}
+		else
+		{
+			for (j = 0; sep[j] != '\0'; j++)
 			{
-				if ((s[i + 1] >= 97 && s[i + 1] <= 122) || (s[0] >= 97 && s[0] <= 122))
+				if (s[i] == sep[j])
 				{
-					s[i + 1] = s[i + 1] - 32;
-					s[0] = s[0] - 32;
+					if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					{	
+						s[i + 1] = s[i + 1] - 32;
+					}
 				}
 			}
 		}
