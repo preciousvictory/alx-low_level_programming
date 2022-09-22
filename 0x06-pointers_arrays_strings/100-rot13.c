20e9f31ebd33ef8f2a8186af5a9ce96ed5cc770a
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-22 05:06:50 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string using rot13.
+ * Input:	ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+ * Output:	NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm
+ * and vice-versa
+ *
+ * @s: string
+ * Return: Rot13 s string
+ */
+
+char *rot13(char *s)
+{
+	int i, n;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		n = 0;
+		while (input[n] != '\0')
+		{
+			if (s[i] == input[n])
+			{
+				s[i] = output[n];
+			}
+
+			n++;
+		}
+	}
+}
