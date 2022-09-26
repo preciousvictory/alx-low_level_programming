af8772c7707c7bc9f20b66d9a7423c19e5b6c063
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-26 08:42:27 -0700

    memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+int _putchar(char c); 
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
