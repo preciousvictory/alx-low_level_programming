bcbe6e63ea2956adfe113524fa273d0be76eab01
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-26 14:58:22 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * _strspn - a function that gets the length of a prefix substring.
  * @s: string tobe checked
  * @accept: contains accepted strings
- * 
+ *
  * Return: the number of bytes in the initial segment of s which consist only
  * of bytes from accept
  */
