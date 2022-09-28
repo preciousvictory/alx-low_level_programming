9ce71ed3e82e337f5e6d56d02c49df6a1ae58f6e
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-28 06:24:57 -0700

    reverse

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -3,10 +3,11 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	_puts_recursion("Puts with recursion");
+	_print_rev_recursion("\nColton Walker");
 	return (0);
 }
