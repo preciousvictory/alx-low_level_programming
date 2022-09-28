5fca340b96d830a165b4ba7453bb6186df3c0406
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-28 15:55:13 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt - finds the square rootof a numbe
+ * @n: the number
+ * @l: loop
+ *
+ * Returns the square root
+ */
+
+int _sqrt(int n, int l)
+{
+	if (l * l == n)
+		return (l);
+	if (l * l != n && l < n)
+		return (_sqrt(n, l + 1));
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - a function that returns the natural square root of anumber
+ * @n: the number
+ *
+ * Return: sqaure root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	if (n > 1)
+		return (_sqrt(n , 2));
+	return (-1);
+}
