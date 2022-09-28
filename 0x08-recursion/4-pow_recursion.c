c9846ecf64d144725f484e0965a31b61b8b253bc
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-28 11:16:17 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -  a function that returns the value of x raised
+ * to the power of y.
+ * @x: base
+ * @y: power
+ *
+ * Return: the answr fr x ** y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int pow = 1;
+
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+	{
+		return(1);
+	}
+	else
+	{
+		pow *=  x * _pow_recursion(x, y - 1);
+	}
+
+	return (pow);
+}
