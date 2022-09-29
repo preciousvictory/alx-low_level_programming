721a0c20148e0f203adede9ec69de31460bcfe75
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-28 19:26:11 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,7 +25,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
-	if (*s2 == '*' && *s1 != *s2)
-		return (0);
+	if (*s2 == '*' && *s1 = '\0')
+		return (wildcmp(s1, s2 + 1));
 	return (0);
 }
