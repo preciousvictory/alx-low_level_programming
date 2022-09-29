6cf073e4397a5867b0b6a7772bc0cb1d7729a930
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-09-28 17:01:57 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that returns the length of a string.
+ * @s: the string
+ *
+ * Return: the length of the  string
+ */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (*s)
+	{
+		l += _strlen_recursion(s + 1) + 1;
+	}
+	return (l);
+}
+
+/**
+ * check - if it is a palindrome
+ * @s: the string
+ * @i: loop check forward
+ * @len: loop check backward
+ *
+ * Return: 1 if it is a palindrome otherwise 0
+ */
+
+int check(char *s, int i, int len)
+{
+	int l;
+
+	l = _strlen_recursion(s);
+
+	if (s[i] != s[len])
+		return (0);
+	if (s[i] == s[len])
+	{
+		if (l / 2 == (i + 1))
+			return (1);
+		else
+			return (check(s, i + 1, len -1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - a function that returns 1 if a string is a palindrome
+ * and 0 if not.
+ * @s: the string
+ *
+ * Return: answer
+ */
+
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s) - 1;
+
+	return (check(s, 0, len));
+}
