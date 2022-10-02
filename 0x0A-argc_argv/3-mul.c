f18a66003cc7595f30e33b971fe4edfaa8481fb9
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-02 16:18:33 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argumrnt vector
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+
+	if (argc == 1)
+		printf("Error");
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+
+	return (0);
+}
