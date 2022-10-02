dad3edf6e671af2bd14607190da4c861f3c68f18
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-02 14:49:15 -0700

    myname is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * main -   a program that prints its name, followed by a new line.
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argumrnt vector
  *
  * Return: 0 (success)
  */
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - a program that prints the number of arguments passed into it.
+ * followed by a new line
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	
+
+	return (0);
+}
