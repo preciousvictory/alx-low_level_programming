f9c0c37437f498670041960c89aa232f366f2ea6
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-02 14:17:17 -0700

    myname is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -   a program that prints its name, followed by a new line.
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+
+	return (0);
+}
