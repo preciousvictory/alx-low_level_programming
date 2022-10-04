e434df75c7c6ee8936ce4f3b58d61f5b62507dda
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-03 17:33:01 -0700

    alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * alloc_grid - a function that returns a pointer to a 2 dimensional
+ * array of integers.
+ * @width: row
+ * @height: column
+ *
+ * - Each element of the grid should be initialized to 0
+ * - The function should return NULL on failure
+ * - If width or height is 0 or negative, return NULL
+ * Return: 2 dimensional array or NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	char **ar;
+	int i;
+
+	len = width * height
+	ar = malloc(len);
+
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		ar[i] = 0;
+	}
+
+	return (ar);
+}
