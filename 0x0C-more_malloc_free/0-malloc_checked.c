d550aa011a34064c22fe4ace009ac6f7fb9cedbc
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-06 09:47:46 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,13 +3,19 @@
 /**
  * malloc_checked - a function that allocates memory using malloc.
  * @b: size of memory
+ * if malloc fails, the malloc_checked termination with a status value of 98
+ *
+ * Return: a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
-	int *str;
+	int *ptr;
 
-	str =  malloc(b);
-	if (str == NULL)
+	ptr =  malloc(b);
+	if (ptr == NULL)
+	{
 		exit(98);
+	}		
+	return (ptr);
 }
