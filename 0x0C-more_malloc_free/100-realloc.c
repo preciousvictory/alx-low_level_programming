83c650e393d46763a52903f142cb75b77d5bc7f8
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-06 17:51:30 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,10 +2,10 @@
 
 /**
  * _realloc - a function that reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated with a call to malloc: 
+ * @ptr: pointer to the memory previously allocated with a call to malloc:
  * malloc(old_size)
  * @old_size: size, in bytes, of the allocated space for ptr
- * new_size: new size, in bytes of the new memory block
+ * @new_size: new size, in bytes of the new memory block
  *
  * Description
  * The contents will be copied to the newly allocated space, in the range from
@@ -24,6 +24,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *n_ptr;
+	unsigned int i;	
 
 	if (new_size == old_size)
 		return (ptr);
@@ -31,15 +32,28 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		n_ptr =  malloc(new_size);
 		if (n_ptr == NULL)
-			return(n_ptr);
+			return (n_ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
+		return (NULL);
 	}
 
-	if (new_size > old_size)
+	if (new_size > old_size || ptr != NULL)
+	{
 		n_ptr = malloc(new_size);
+		if (n_ptr == NULL)
+			return (NULL);
+
+		/* Copy the values in ptr to n_ptr */
+		i = 0;
+		while (i < old_size)
+		{
+			*((char *)n_ptr + i) = *((char *)ptr + i);
+			i++;
+		}
+	}
 
 	return (n_ptr);
 }
