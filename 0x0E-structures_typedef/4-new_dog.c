f4e297101a15984ce94c7f12e965f816743eaaa7
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-11 04:01:33 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,52 +9,50 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *my_dog;
 	int i, j, l;
 
-	dog_t *my_dog;
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
+	my_dog = malloc(sizeof(*my_dog));
+	if (my_dog == NULL)
+	{
+		return (NULL);
+	}
 
-	my_dog = malloc(sizeof(my_dog));
-	if (my_dog == NULL || owner == NULL || name == NULL)
-		{
-			free(my_dog);
-			return (NULL);
-		}
-
-	for (i = 0; name[i]; i++)
+	for (i = 0; *name++; i++)
 		;
-	for (j = 0; owner[i]; j++)
+	for (j = 0; *owner++; j++)
 		;
 
 	my_dog->name = malloc((i + 1) * sizeof(char));
-	my_dog->owner= malloc((j + 1) * sizeof(char));
-
-	if (my_dog->name == NULL || my_dog->owner == NULL)
+	if (my_dog->name == NULL)
 	{
 		free(my_dog->name);
-		free(my_dog->owner);
-		free(my_dog);
+		return (NULL);
+	}
+
+	my_dog->owner = malloc((j + 1) * sizeof(char));
+	if (my_dog->owner == NULL)
+	{
 		return (NULL);
 	}
 	
-	l = 0;
-	while (l < i)
+
+	for (l = 0; l < i; l++)
 	{
 		my_dog->name[i] =  name[i];
-		l++;
 	}
-	my_dog->name[i] = '\0';
-
-	
-	l = 0;
-	while (l < j)
-	{
-		my_dog->owner[j] = owner[i];
-		l++;
-	}
-	my_dog->owner[j] = '\0';
+	my_dog->name[l] = '\0';
 
 	my_dog->age = age;
 
-	return my_dog;
+	for (l = 0; l < j; l++)
+	{
+		my_dog->owner[j] = owner[i];
+	}
+	my_dog->owner[l] = '\0';
+
+	return (my_dog);
 }
