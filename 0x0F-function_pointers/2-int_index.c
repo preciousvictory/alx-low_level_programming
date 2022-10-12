9a22c204f7d5d26af1a9a3c898afe0fe1124d41f
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-12 14:35:10 -0700

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer.
+ * @array: array to be printed
+ * @size: the number of elements in the array array
+ * @cmp: pointer to the function to be used to compare values
+ *
+ *
+ * Description
+ * int_index returns the index of the first element for which the cmp function
+ * does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
+ *
+ * Return: index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+		}
+		return (i);
+	}
+	return (-1);
+}
