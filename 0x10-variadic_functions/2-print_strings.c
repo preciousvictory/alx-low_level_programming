0a70fc0eeb281da07d8dfe8c4be31de12986eb19
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-13 16:19:36 -0700

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - a function that prints strings, followed by a new line.
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ *
+ * If separator is NULL, don’t print it
+ * If one of the string is NULL, print (nil) instead
+ * Print a new line at the end of your function
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+	unsigned int i;
+	char *s;
+
+	va_start(str, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(str, char *);
+		if (s != NULL)
+		{
+			printf("%s", s);
+		}
+		else
+		{
+			printf("(nil)");;
+		}
+
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(str);
+	putchar('\n');
+}
