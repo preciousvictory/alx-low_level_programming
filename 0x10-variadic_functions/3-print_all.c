95b9e6940c3a159aefc38ea2a78064fd81cbad6b
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-14 04:15:10 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,105 @@
+#include "variadic_functions.h"
+
+void print_char(va_list str);
+void print_int(va_list str);
+void print_float(va_list str);
+void print_string(va_list str);
+void print_all(const char * const format, ...);
+
+/**
+ * print_char - prints characters of type char
+ * @str: declaration of variadic function
+ */
+void print_char(va_list str)
+{
+	char l;
+
+	l = va_arg(str, int);
+	printf("%c", l);
+}
+
+/**
+ * print_int - prints natural numbers of type int
+ * @str: declaration of variadic function
+ */
+void print_int(va_list str)
+{
+	int i;
+
+	i = va_arg(str, int);
+	printf("%d", i);
+}
+
+/**
+ * print_float - prints numbers with decimal points
+ * @str: declaration of variadic function
+ */
+void print_float(va_list str)
+{
+	float f;
+
+	f = va_arg(str, double);
+	printf("%f", f);
+}
+
+/**
+ * print_string - print array of charcters of type char
+ * @str: declaration of variadic function
+ */
+void print_string(va_list str)
+{
+	char *s;
+
+	s = va_arg(str, char *);
+	if (s != NULL)
+		printf("%s", s);
+	else
+		printf("(nil)");
+}
+
+/**
+ * print_all - a function that prints anything.
+ * @format: a list of types of arguments passed to the function
+ *   - c: char
+ *   - i: integer
+ *   - f: float
+ *   - s: char * (if the string is NULL, print (nil) instead
+ *   - any other char should be ignored
+ *
+ *   Print a new line at the end of your function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list str;
+	int i, l;
+	p_all next[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	void (*func)(va_list);
+
+	va_start(str, format);
+
+	l = 0;
+	while (format[l] != '\0')
+	{
+		i = 0;
+		while (i < 4 && *next[i].p != format[l])
+			i++;
+		if (next[i].print != NULL)
+		{
+			func = next[i].print;
+			func(str);
+
+			if (l != 3)
+				printf(", ");
+		}
+		l++;
+	}
+
+	va_end(str);
+	putchar('\n');
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,22 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
+/**
+ * struct prints - a struct used to print
+ * @p: specifies the datatype to be printed
+ * @print: function pointer to a function that prints according to the datatype
+ */
+typedef struct prints
+{
+	char *p;
+	void (*print)(va_list str);
+} p_all;
+
+void print_char(va_list str);
+void print_int(va_list str);
+void print_float(va_list str);
+void print_string(va_list str);
+
+void print_all(const char * const format, ...);
 
 #endif
