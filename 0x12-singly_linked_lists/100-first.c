b2ecf90cd9e01ea31cb1caa48e4df78a6c397fec
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-20 21:11:44 -0700

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 void __attribute__((constructor)) start(void);
 
+/**
+ * start - executes this function before the main() function
+ */
 void start(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
