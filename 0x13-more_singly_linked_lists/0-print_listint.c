ea50df27758f976bbc9ef6cbd3d22483534dc5ff
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-25 07:25:01 -0700

    printlistint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints all the elements of a listint_t list.
+ * @h: singly linked list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
