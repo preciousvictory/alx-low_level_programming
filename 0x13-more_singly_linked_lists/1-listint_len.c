17878482b713df993176ef90be2bde60a7bb4de4
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-25 07:33:52 -0700

    listintlen

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * listint_len - a function that returns the number of elements in a linked listint_t list.
+ * listint_len - a function that returns the number of elements in a linked
+ * listint_t list.
  * @h: singly linked list
  *
  * Return: the numberof elements in a linked listint_t list.
