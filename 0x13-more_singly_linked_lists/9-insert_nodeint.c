c6164faea9384d14429e833eacdcd8d67452c286
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-26 07:56:13 -0700

    add in index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -47,7 +47,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 		current = current->next;
 	}
-
+	if (i == idx)
+	{
+		new_node->next = current->next;
+		current->next = new_node;
+	}
 	free(new_node);
 	return (NULL);
 }
