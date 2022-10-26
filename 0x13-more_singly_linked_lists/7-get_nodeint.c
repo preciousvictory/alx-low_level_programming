5f5b3ef8569d54cf3e38246b1ef588133d16398f
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-26 06:39:00 -0700

    return

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - a function that returns the nth node of a
+ * listint_t linked list.
+ * @index:  is the index of the node, starting at 0
+ * @head: singly linked list
+ *
+ * Return: the nth node of a listint_t linked list.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+
+		i++;
+		head = head->next;
+	}
+
+	return (NULL);
+}
