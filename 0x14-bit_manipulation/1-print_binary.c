22e23726343a4a095e1d05e6d33246a29c5aba5a
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-28 10:40:08 -0700

    bitwise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,12 +6,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		_putchar('0');
-
-	while (n)
+	if (n > 1)
 	{
-		_putchar((n & 1) + '0');
-		n = n >> 1;
+		print_binary(n >> 1);
 	}
+	_putchar((n & 1) + '0');
 }
