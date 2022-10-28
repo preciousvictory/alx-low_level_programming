b09461e5907807c2f3a3ef0ac11c6c3ba55f59df
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-28 11:48:59 -0700

    bitwise

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - a function that sets the value of a bit to 1 at a given index.
+ * @n: number
+ * @index: is the index, starting from 0 of the bit you want to set
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int d;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
+	d = 1 << index;
+	*n = *n | d;
+
+	return (1)
+}
