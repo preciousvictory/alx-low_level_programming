168945f69ca7f6fa40a473acd3ece00d42bca49f
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-10-31 12:05:38 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,20 +23,21 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
 	{
-		close(fd);
-		return (1);
+		text_content = "";
 	}
 
 	n = sizeof(text_content);
 	w = write(fd, text_content, n);
 	if (w == -1)
 		return (-1);
+
+	close(fd);
 
 	return (1);
 }
