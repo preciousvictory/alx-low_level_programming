44fdc0288a86855f5042718391434f84f5ddfd4e
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-06 14:01:50 -0800

    hashnode

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,7 +5,8 @@
  * @key :  is the key
  * @size:  is the size of the array of the hash table
  *
- * Return: the index at which the key/value pair should be stored in the array of the hash table
+ * Return: the index at which the key/value pair should be stored in the array
+ * of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
@@ -13,8 +14,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	 * This function should use the hash_djb2 function that in the 1-djb2.c file
 	 *
 	 * the hash function is used to get the index
-	 * If we are inserting the key for the first time, the item must be a NULL. 
-	 *
+	 * If we are inserting the key for the first time, the item must be a NULL.
 	 * Returns the index at which the key/value pair should be stored in the array
 	 * of the hash table
 	 */
