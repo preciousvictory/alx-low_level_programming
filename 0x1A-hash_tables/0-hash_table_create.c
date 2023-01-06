0597961bc4f94989ec7574086afa5635fe756511
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-06 12:27:53 -0800

    hashnode

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,12 +4,19 @@
  * hash_table_create - a function that creates a hash table.
  *
  * @size: is the size of the array
- * 
+ *
  * Return:  a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));	
+	/**
+	 * creates a hash table.
+	 *
+	 * assign NULL as initial value for each node
+	 * Each cell of this array is a pointer to the first node of a linked list
+	 */
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
 	if (table == NULL)
 	{
@@ -18,6 +25,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < table->size; i++)
+	{
+		table->array = NULL;
+	}
 
 	return (table);
 }
