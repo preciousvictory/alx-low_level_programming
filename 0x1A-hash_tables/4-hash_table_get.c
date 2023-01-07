f49755b568ddad520cb03f2d075b696f676f143e
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-06 17:44:15 -0800

    hashnode

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,22 +3,27 @@
  * hash_table_get - a function that retrieves a value associated with a key.
  * @ht: is the hash table you want to look into
  * @key: is the key you are looking for
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * Return: the value associated with the element, or NULL if key couldn’t be
+ * found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *item;
 
+	if (ht == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	item = ht->array[index];
 
-	if (item != NULL)
+	while (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
 		{
-			return item->value;
+			return (item->value);
 		}
+		item = ite->next;
 	}
-	return NULL;
+	return (NULL);
 }
