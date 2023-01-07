902fa9238e787a1e375a655bcfb93078e8ae0d27
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-06 16:41:31 -0800

    hashbnode

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,6 +41,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	item->value = strdup(value);
 	item->next =  ht->array[index];
 	ht->array[index] = item;
-	
+
 	return (1);
 }
