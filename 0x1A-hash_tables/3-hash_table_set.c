3baaff89ec0879d2db91186a04cec8a4a228cbdf
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-06 17:20:57 -0800

    hashnode

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,6 @@
 #include "hash_tables.h"
 /**
  * hash_table_set - a function that adds an element to the hash table.
- *
  * @ht:  is the hash table you want to add or update the key/value to
  * @key:  is the key. key can not be an empty string
  * @value:  is the value associated with the key. value must be duplicated.
@@ -14,27 +13,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *item;
-       	hash_node_t *current_item;
+	hash_node_t *current_item;
 
 	if (ht == NULL || key == NULL || value == NULL)
-	{
 		return (0);
-	}
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	current_item = ht->array[index];
 	if (current_item == NULL)
 	{
 		item = malloc(sizeof(hash_node_t));
 		if (item == NULL)
-		{
 			return (0);
-		}
-
-		/**
-		 * Creates an item (node of hash table)
-		 */
+		/* Creates an item (node of hash table) */
 		item->key = strdup(key);
 		item->value = strdup(value);
 		item->next = NULL;
@@ -42,12 +33,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = item;
 		return (1);
 	}
-
 	while (current_item != NULL)
 	{
-		/**
-		 * if item exits replace it
-		 */
+		/* if item exits replace it */
 		if (strcmp(key, current_item->key) == 0)
 		{
 			free(current_item->value);
@@ -56,15 +44,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current_item = current_item->next;
 	}
-
+	/* if it doesn't have it creates a new item */
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
-	{
 		return (0);
-	}
-
 	item->next = ht->array[index];
 	ht->array[index] = item;
-
 	return (1);
 }
