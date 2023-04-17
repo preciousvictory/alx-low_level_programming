532e7e13996a6c7336acfe94fa6c8a31aeb72cf3
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-16 21:54:37 -0700

    search algorithm

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - a function that searches for a value in a sorted
+ * array of integers using the Interpolation search algorithm
+ *
+ * binary_search - a function that searches for a value in a sorted array of
+ * integers using the Binary search algorithm
+ *
+ * @array:  is a pointer to the first element of the array to search in
+ * @size:  is the number of elements in array
+ * @value:  is the value to search for
+ *
+ * If value is not present in array or if array is NULL, your function
+ * must return -1
+ *
+ * Return: the first index where value is located
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, L, R;
+
+	if (array == NULL)
+		return (-1);
+	
+	for (l = 0, r = size - 1; r >= l;) 
+	{
+		pos = L + (((double)(R - L) / (array[R] - array[L])) * (value - array[L]));
+		if (pos < size)
+			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", pos);
+			break;
+		}
+
+		if (array[pos] == value)
+			return (pos);
+		if (array[pos] > value)
+			R = pos - 1;
+		else
+			L = pos + 1;
+	}
+
+	return (-1);
+}
